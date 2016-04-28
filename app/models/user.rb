ac517c83be7a3a8712a918c372be492ef1b39174
josephwilliams <josephwilli12@gmail.com>
2016-04-28 15:48:14 -0700

    fix again

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,7 @@ class User < ActiveRecord::Base
   after_initialize :ensure_session_token
   # before_validation :ensure_session_token_uniqueness
 
-  has_many(
-    :photos,
 
-  )
 
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
