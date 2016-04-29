06ae56f1c986f7a7f49fa399db070f455901f93a
josephwilliams <josephwilli12@gmail.com>
2016-04-29 10:14:22 -0700

    heroku push attempt

---
+++ frontend/components/auth/FunForm.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var FunForm = React.createClass({
+  render: function () {
+    return (
+      <div id="toggleProfile">
+        auth forms
+      </div>
+
+    );
+  }
+});
+
+module.exports = FunForm;
--- frontend/components/splash/SplashPage.jsx
+++ frontend/components/splash/SplashPage.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var Modal = require("react-modal");
 var SplashImage = require('./SplashImage');
 var SplashContent = require('./SplashContent');
+var FunForm = require('../auth/FunForm');
 
 var SplashPage = React.createClass({
   render: function () {
@@ -9,6 +10,7 @@ var SplashPage = React.createClass({
       <div>
         <SplashImage/>
         <SplashContent/>
+        <FunForm/>
       </div>
     );
   }
